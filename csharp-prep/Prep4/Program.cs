1509cf6854a917b07482fb2f2dde138a873f369f
dalmurd <mur21006@byui.edu>
2025-09-24 09:36:40 -0600

    Sum of Numbers

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int number;
+        List<int> numbers = new List<int>();
+
+        do
+        {
+            Console.WriteLine("Enter a number (or 0 to stop):");
+            number = int.Parse(Console.ReadLine());
+            numbers.Add(number);
+
+        } while (number != 0);
+
+
+        Console.WriteLine($"The sum is: {numbers.Sum()}");
+        Console.WriteLine($"The average is: {numbers.Average()}");
+        Console.WriteLine($"The largest number is: {numbers.Max()}");
+           
+            
+
     }
 }
