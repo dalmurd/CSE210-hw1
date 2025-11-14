519bf264af0bd1e09595591b971fe8a6d0e7df47
dalmurd <mur21006@byui.edu>
2025-11-13 17:30:02 -0700

    Shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Shape shape = new Square("Green;", 2.0);
+        Console.WriteLine($"Color: {shape.GetColor()} Area: {shape.GetArea()} ");
+
+        Shape notSquare = new Rectangle("Red;", 3.0, 4.0);
+        Console.WriteLine($"Color: {notSquare.GetColor()} Area: {notSquare.GetArea()}");
+
+        Shape circle = new Circle("Blue;", 2.0);
+        Console.WriteLine($"Color: {circle.GetColor()} Area: {circle.GetArea():F2}"); // F2 rounds the decimal to 2 decimal places 
+
+
+        Console.WriteLine("------List of Shapes ------");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(shape);
+        shapes.Add(notSquare);
+        shapes.Add(circle);
+        foreach (Shape s in shapes)
+        { Console.WriteLine($"Color: {s.GetColor()} Area: {s.GetArea():F2}"); }
+        
     }
 }
